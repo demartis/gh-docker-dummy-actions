26279705d3f0dca5b5f4bbd144eca3c1833c17f5
Riccardo De Martis <riccardo@demartis.it>
2023-11-15 17:07:38 +0100

    dev

--- test/mcr_version.sh
+++ test/mcr_version.sh
@@ -1,13 +1,16 @@
-cat $MCR_MASTER_PATH/VersionInfo.xml
 
-XMLTAG=release
-result=`sed -n "/$XMLTAG/{s/.*<$XMLTAG>//;s/<\/$XMLTAG.*//;p;}" $MCR_MASTER_PATH/VersionInfo.xml`
+echo $VARIAB
 
-echo "Checking MCR version. It must be $1"
-if [[ "$result" == "$1" ]]; then
-  echo "OK"
-else
-  echo -e '\033[31m' "Error. Version found: $result" '\033[0m'
-  exit 1
-fi
+#cat $MCR_MASTER_PATH/VersionInfo.xml
+#
+#XMLTAG=release
+#result=`sed -n "/$XMLTAG/{s/.*<$XMLTAG>//;s/<\/$XMLTAG.*//;p;}" $MCR_MASTER_PATH/VersionInfo.xml`
+#
+#echo "Checking MCR version. It must be $1"
+#if [[ "$result" == "$1" ]]; then
+#  echo "OK"
+#else
+#  echo -e '\033[31m' "Error. Version found: $result" '\033[0m'
+#  exit 1
+#fi
 
